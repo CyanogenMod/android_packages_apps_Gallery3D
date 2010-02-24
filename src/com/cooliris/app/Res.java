95dcb300eebab75a3d915a5e8e2541c3e8732f84
Chih-Chung Chang <chihchung@google.com>
2010-02-24 11:20:14 -0800

    Add missing Res.java

---
+++ src/com/cooliris/app/Res.java
@@ -0,0 +1,22 @@
+package com.cooliris.app;
+
+import com.cooliris.app.R;
+
+public class Res {
+//    	public static final R.anim anim = new R.anim();
+    	public static final R.array array = new R.array();
+    	public static final R.attr attr = new R.attr();
+//    	public static final R.bool bool = new R.bool();
+//    	public static final R.color color = new R.color();
+//    	public static final R.dimen dimen = new R.dimen();
+    	public static final R.drawable drawable = new R.drawable();
+    	public static final R.id id = new R.id();
+//    	public static final R.integer integer = new R.integer();
+    	public static final R.layout layout = new R.layout();
+//    	public static final R.plurals plurals = new R.plurals();
+//    	public static final R.raw raw = new R.raw();
+    	public static final R.string string = new R.string();
+//    	public static final R.style style = new R.style();
+//    	public static final R.styleable = new R.styleable();
+    	public static final R.xml xml = new R.xml();
+}
