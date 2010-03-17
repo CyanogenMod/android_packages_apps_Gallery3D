df9ffa0cbaf24e36ac84b086a6262b456f3e316b
Venkat <venkat@cooliris.com>
2010-03-16 17:02:40 -0700

    Fix for #2511402 where the camera stack still persisted in the cache even after it was deleted from the database.

--- src/com/cooliris/cache/CacheService.java
+++ src/com/cooliris/cache/CacheService.java
@@ -1016,6 +1016,27 @@ public final class CacheService extends IntentService {
                         }
                         ++ctr;
                     } while (ctr < numSets);
+                    // We now check for any deleted sets.
+                    final byte[] albumData = sAlbumCache.get(ALBUM_CACHE_METADATA_INDEX, 0);
+                    if (albumData != null && albumData.length > 0) {
+                        final DataInputStream dis = new DataInputStream(new BufferedInputStream(new ByteArrayInputStream(albumData), 256));
+                        try {
+                            final int numAlbums = dis.readInt();
+                            for (int i = 0; i < numAlbums; ++i) {
+                                final long setId = dis.readLong();
+                                Utils.readUTF(dis);
+                                dis.readBoolean();
+                                dis.readBoolean();
+                                if (!setIds.contains(setId)) {
+                                    // This set was deleted, we need to recompute the cache.
+                                    markDirty();
+                                    break;
+                                }
+                            }
+                        } catch (Exception e) {
+                            ;
+                        }
+                    }
                 }
             } finally {
                 cursor.close();
