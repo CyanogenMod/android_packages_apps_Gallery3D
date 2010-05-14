9d3bcdb40fef7b35a69f559259f8d981f85c8c44
Wei-Ta Chen <weita@google.com>
2010-05-14 16:17:04 +0800

    Create an initial placeholder for the new 3D gallery code.
    Change-Id: I4c451242303252db8b96ec3c1c86a1330fa7af03

---
+++ new3d/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := GalleryNew3D
+LOCAL_CERTIFICATE := media
+
+LOCAL_OVERRIDES_PACKAGES := Gallery
+
+LOCAL_SDK_VERSION := current
+
+include $(BUILD_PACKAGE)
+
+# Use the following include to make our test apk.
+include $(call all-makefiles-under,$(LOCAL_PATH))
---
+++ new3d/src/com/android/gallery3d/app/Gallery.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.gallery3d.app;
+
+import com.android.gallery3d.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public final class Gallery extends Activity {
+    public static final String REVIEW_ACTION = "com.android.gallery3d.app.REVIEW";
+    private static final String TAG = "Gallery";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Log.i(TAG, "onCreate");
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+    private boolean isPickIntent() {
+        String action = getIntent().getAction();
+        return (Intent.ACTION_PICK.equals(action) || Intent.ACTION_GET_CONTENT.equals(action));
+    }
+
+    private boolean isViewIntent() {
+        String action = getIntent().getAction();
+        return Intent.ACTION_VIEW.equals(action);
+    }
+
+    private boolean isReviewIntent() {
+        String action = getIntent().getAction();
+        return REVIEW_ACTION.equals(action);
+    }
+
+    private boolean isImageType(String type) {
+        return type.contains("*/") || type.equals("vnd.android.cursor.dir/image") || type.equals("image/*");
+    }
+
+    private boolean isVideoType(String type) {
+        return type.contains("*/") || type.equals("vnd.android.cursor.dir/video") || type.equals("video/*");
+    }
+}
