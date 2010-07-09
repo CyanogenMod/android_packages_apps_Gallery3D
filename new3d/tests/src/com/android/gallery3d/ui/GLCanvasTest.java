3cbb44963568b0b0af4640c5bedb75039aafa6e6
Chih-Chung Chang <chihchung@google.com>
2010-07-09 12:16:41 +0800

    Add more tests for transformation in GLCanvas.
    Change-Id: Ib0a8cbb39e3635ae10e84750e4795a563e6dad12

--- new3d/tests/src/com/android/gallery3d/ui/GLCanvasTest.java
+++ new3d/tests/src/com/android/gallery3d/ui/GLCanvasTest.java
@@ -94,10 +94,12 @@ public class GLCanvasTest extends TestCase {
         int green,
         int blue,
         int alpha) {
-        final float X = 65535f;
+        final float X = 65536f;
         return makeColor4f(red / X, green / X, blue / X, alpha / X);
     }
 
+    // This test assumes we use pre-multipled alpha blending and should
+    // set the blending function and color correctly.
     private static class SetColorTest extends GLStub {
         private int mCalled = 0;
         private int mCalledColor;
@@ -131,13 +133,13 @@ public class GLCanvasTest extends TestCase {
         }
 
         void run() {
-            // This test assumes we use pre-multipled alpha blending.
             int[] testColors = new int[] {
                 0, 0xFFFFFFFF, 0xFF000000, 0x00FFFFFF, 0x80FF8001,
                 0x7F010101, 0xFEFEFDFC, 0x017F8081, 0x027F8081, 0x2ADE4C4D
             };
 
             GLCanvas canvas = new GLCanvasImp(this);
+            canvas.setSize(400, 300);
             // Test one color to make sure blend is called.
             assertEquals(0, mCalled);
             canvas.bindColor(0x7F804020);
@@ -237,6 +239,7 @@ public class GLCanvasTest extends TestCase {
 
         void run() {
             GLCanvas canvas = new GLCanvasImp(this);
+            canvas.setSize(400, 300);
 
             assertEquals(0, mCalled);
             canvas.bindColor(0xFF804020);
@@ -254,6 +257,9 @@ public class GLCanvasTest extends TestCase {
         new DrawLineTest().run();
     }
 
+    // This test assumes the drawLine() function use glDrawArrays() with
+    // LINE_STRIP mode to/ draw the line and the input coordinates are used
+    // directly.
     private static class DrawLineTest extends GLStub {
         private int mCalled = 0;
         private int mCalledColor;
@@ -269,21 +275,23 @@ public class GLCanvasTest extends TestCase {
         @Override
         public void glDrawArrays(int mode, int first, int count) {
             assertNotNull(mVertexPointer);
+            assertEquals(GL10.GL_LINE_STRIP, mode);
             assertEquals(2, count);
             mVertexPointer.bindByteBuffer();
 
             double[] coord = new double[4];
             mVertexPointer.getArrayElement(first, coord);
-            mResult[0] = (int)coord[0];
-            mResult[1] = (int)coord[1];
+            mResult[0] = (int) coord[0];
+            mResult[1] = (int) coord[1];
             mVertexPointer.getArrayElement(first + 1, coord);
-            mResult[2] = (int)coord[0];
-            mResult[3] = (int)coord[1];
+            mResult[2] = (int) coord[0];
+            mResult[3] = (int) coord[1];
             mCalled++;
         }
 
         void run() {
             GLCanvas canvas = new GLCanvasImp(this);
+            canvas.setSize(400, 300);
             canvas.drawLine(2, 7, 1, 8);
             assertEquals(1, mCalled);
 
@@ -296,9 +304,149 @@ public class GLCanvasTest extends TestCase {
     }
 
     @SmallTest
+    public void testTransform() {
+        new TransformTest().run();
+    }
+
+    // This test assumes glLoadMatrixf is used to load the model view matrix,
+    // and glOrthof is used to load the projection matrix.
+    //
+    // The projection matrix is set to an orthogonal projection which is the
+    // inverse of viewport transform. So the model view matrix maps input
+    // directly to screen coordinates (default no scaling, and the y-axis is
+    // reversed).
+    //
+    // The matrix here are all listed in column major order.
+    //
+    private static class TransformTest extends GLStub {
+        private int mCurrentMatrixMode = GL10.GL_MODELVIEW;
+        private float[] mModelViewMatrix = new float[16];
+        private float[] mModelViewMatrixUsed = new float[16];
+        private float[] mProjectionMatrix = new float[16];
+        private float[] mProjectionMatrixUsed = new float[16];
+
+        @Override
+        public void glMatrixMode(int mode) {
+            mCurrentMatrixMode = mode;
+        }
+
+        @Override
+        public void glLoadMatrixf(float[] m, int offset) {
+            if (mCurrentMatrixMode == GL10.GL_MODELVIEW) {
+                System.arraycopy(m, offset, mModelViewMatrix, 0, 16);
+            } else if (mCurrentMatrixMode == GL10.GL_PROJECTION) {
+                System.arraycopy(m, offset, mProjectionMatrix, 0, 16);
+            }
+        }
+
+        @Override
+        public void glDrawArrays(int mode, int first, int count) {
+            copy(mModelViewMatrixUsed, mModelViewMatrix);
+            copy(mProjectionMatrixUsed, mProjectionMatrix);
+        }
+
+        @Override
+        public void glOrthof(
+            float left,
+            float right,
+            float bottom,
+            float top,
+            float zNear,
+            float zFar ) {
+                float tx = - (right + left) / (right - left);
+                float ty = - (top + bottom) / (top - bottom);
+                float tz = - (zFar + zNear) / (zFar - zNear);
+                float[] m = new float[] { 2/(right - left), 0, 0,  0,
+                                          0, 2/(top - bottom), 0,  0,
+                                          0, 0, -2/(zFar - zNear), 0,
+                                          tx, ty, tz, 1
+                                        };
+                glLoadMatrixf(m, 0);
+        }
+
+        private void assertMatrixEq(float[] expected, float[] actual) {
+            Log.v(TAG, "expected = " + Arrays.toString(expected) +
+                    ", actual = " + Arrays.toString(actual));
+
+            for (int i = 0; i < 16; i++) {
+                assertFloatEq(expected[i], actual[i]);
+            }
+        }
+
+        private void copy(float[] dest, float[] src) {
+            System.arraycopy(src, 0, dest, 0, 16);
+        }
+
+        void run() {
+            GLCanvas canvas = new GLCanvasImp(this);
+            canvas.setSize(40, 50);
+
+            // Initial matrix
+            canvas.drawLine(0, 0, 1, 1);
+            assertMatrixEq(new float[] {1,  0, 0, 0,
+                                        0, -1, 0, 0,
+                                        0,  0, 1, 0,
+                                        0, 50, 0, 1},
+                    mModelViewMatrixUsed);
+
+            assertMatrixEq(new float[] {2f/40,    0,  0,  0,
+                                            0,2f/50,  0,  0,
+                                            0,    0, -1,  0,
+                                           -1,   -1,  0,  1},
+                    mProjectionMatrixUsed);
+
+            // Translation
+            canvas.translate(3, 4, 5);
+            canvas.drawLine(0, 0, 1, 1);
+            assertMatrixEq(new float[] {1,  0, 0, 0,
+                                        0, -1, 0, 0,
+                                        0,  0, 1, 0,
+                                        3, 46, 5, 1},
+                    mModelViewMatrixUsed);
+            canvas.save();
+
+            // Scaling
+            canvas.scale(0.7f, 0.6f, 0.5f);
+            canvas.drawLine(0, 0, 1, 1);
+            assertMatrixEq(new float[] {0.7f,     0,    0, 0,
+                                           0, -0.6f,    0, 0,
+                                           0,     0, 0.5f, 0,
+                                           3,    46,    5, 1},
+                    mModelViewMatrixUsed);
+
+            // Rotation
+            canvas.rotate(90, 0, 0, 1);
+            canvas.drawLine(0, 0, 1, 1);
+            assertMatrixEq(new float[] {    0, -0.6f,    0, 0,
+                                        -0.7f,     0,    0, 0,
+                                            0,     0, 0.5f, 0,
+                                            3,    46,    5, 1},
+                    mModelViewMatrixUsed);
+            canvas.restore();
+
+            // After restoring to the point just after translation,
+            // do rotation again.
+            canvas.rotate(180, 1, 0, 0);
+            canvas.drawLine(0, 0, 1, 1);
+            assertMatrixEq(new float[] {  1,  0,  0, 0,
+                                          0,  1,  0, 0,
+                                          0,  0, -1, 0,
+                                          3, 46,  5, 1},
+                    mModelViewMatrixUsed);
+        }
+    }
+
+    @SmallTest
     public void testGetGLInstance() {
         GL11 glStub = new GLStub();
         GLCanvas canvas = new GLCanvasImp(glStub);
         assertSame(glStub, canvas.getGLInstance());
+    }
+
+    public static void assertFloatEq(float expected, float actual) {
+        if (Math.abs(actual - expected) > 1e-6) {
+            Log.v(TAG, "expected: " + expected + ", actual: " + actual);
+            fail();
+        }
     }
 }
