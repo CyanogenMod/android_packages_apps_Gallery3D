b99ad1a872673ddb626f75bba6cfa178a236c9f7
Jack Palevich <jackpal@google.com>
2010-08-20 14:06:48 -0700

    Tilt code now handles displays with a default landscape orientation.
    Based on Id7baafa7465aba938a4fec1f165ef6fe9d8af8e1
    
    Bug: 2937359
    Change-Id: I6ceb5763edd837f08239b88ad947e24801fbb537

--- src/com/cooliris/media/GridInputProcessor.java
+++ src/com/cooliris/media/GridInputProcessor.java
@@ -20,9 +20,12 @@ import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.os.SystemClock;
+import android.view.Display;
 import android.view.GestureDetector;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
+import android.view.Surface;
+import android.view.WindowManager;
 
 import com.cooliris.app.App;
 
@@ -57,6 +60,7 @@ public final class GridInputProcessor implements GestureDetector.OnGestureListen
     private boolean mTouchFeedbackDelivered;
     private GestureDetector mGestureDetector;
     private ScaleGestureDetector mScaleGestureDetector;
+    private Display mDisplay;
     private boolean mZoomGesture;
     private int mCurrentScaleSlot;
     private float mScale;
@@ -76,6 +80,10 @@ public final class GridInputProcessor implements GestureDetector.OnGestureListen
         mGestureDetector.setIsLongpressEnabled(true);
         mZoomGesture = false;
         mScale = 1.0f;
+        {
+            WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            mDisplay = windowManager.getDefaultDisplay();
+        }
     }
 
     public int getCurrentFocusSlot() {
@@ -109,7 +117,23 @@ public final class GridInputProcessor implements GestureDetector.OnGestureListen
         switch (event.sensor.getType()) {
         case Sensor.TYPE_ACCELEROMETER:
             float[] values = event.values;
-            float valueToUse = (mCamera.mWidth < mCamera.mHeight) ? values[0] : -values[1];
+            float valueToUse;
+            switch (mDisplay.getRotation()) {
+            case Surface.ROTATION_0:
+                valueToUse = values[0];
+                break;
+            case Surface.ROTATION_90:
+                valueToUse = -event.values[1];
+                break;
+            case Surface.ROTATION_180:
+                valueToUse = -event.values[0];
+                break;
+            case Surface.ROTATION_270:
+                valueToUse =  event.values[1];
+                break;
+            default:
+                valueToUse = 0.0f;
+            }
             float tiltValue = 0.8f * mPrevTiltValueLowPass + 0.2f * valueToUse;
             if (Math.abs(tiltValue) < 0.5f)
                 tiltValue = 0.0f;
