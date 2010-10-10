340f2a324dc2b6c07d908136716548a34dbf4b37
Petr Havlena <havlenapetr@gmail.com>
2010-10-10 14:07:27 +0200

    add menu for connecting to rtsp or http stream
    Change-Id: I518318b3744c00ca73a88ac87ac10712a6d3d857

--- src/com/cooliris/media/MovieView.java
+++ src/com/cooliris/media/MovieView.java
@@ -17,13 +17,19 @@
 package com.cooliris.media;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.net.Uri;
 
 import com.cooliris.app.App;
 import com.cooliris.app.Res;
@@ -34,6 +40,9 @@ import com.cooliris.app.Res;
 public class MovieView extends Activity {
     @SuppressWarnings("unused")
     private static final String TAG = "MovieView";
+
+    // IDs of the main menu items.
+    public static final int MENU_OPEN_URL          = 0;
 
     private App mApp = null; 
     private MovieViewControl mControl;
@@ -71,20 +80,71 @@ public class MovieView extends Activity {
     public void onPause() {
         mControl.onPause();
         super.onPause();
-    	mApp.onPause();
+        mApp.onPause();
     }
 
     @Override
     public void onResume() {
         mControl.onResume();
         super.onResume();
-    	mApp.onResume();
+        mApp.onResume();
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        menu.clear();
+
+        MenuItem item1 = menu.add(0, MENU_OPEN_URL, 0, R.string.menu_open_url);
+        item1.setIcon(android.R.drawable.ic_menu_preferences);
+
+        return true;
+    }
+
+    private void restart(String uri) {
+        Intent i = new Intent(this, MovieView.class);
+        i.setData(Uri.parse(uri));
+        startActivity(i);
+    }
+
+    private void showSetUrlDialog() {
+        final EditText editText = new EditText(this);
+        //editText.setText("rtsp://192.168.1.1/sample_300kbit.mp4");
+        editText.setHint(R.string.video_set_url_hint);
+
+        new AlertDialog.Builder(this)
+        .setView(editText)
+        .setTitle(R.string.video_set_url_title)
+        .setMessage(R.string.video_set_url_msg)
+        .setPositiveButton(R.string.video_set_url_pos, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                String uri = editText.getText().toString();
+                finish();
+                restart(uri);
+            }
+        })
+        .setNegativeButton(R.string.video_set_url_neg, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        }).create().show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        boolean ret = true;
+        switch(item.getItemId()) {
+            case MENU_OPEN_URL:
+                showSetUrlDialog();
+                break;
+            default:
+                ret = false;
+        }
+        return ret;
     }
     
     @Override
     public void onDestroy() {
         mControl.onDestroy();
-    	mApp.shutdown();
-    	super.onDestroy();
+        mApp.shutdown();
+        super.onDestroy();
     }
 }
